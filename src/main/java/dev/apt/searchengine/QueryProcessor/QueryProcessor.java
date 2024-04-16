84e12ceaf7fc60a4a87fdf5e7afe1e7503df7cda
Adham-Hussin <adhamhusn@gmail.com>
2024-04-16 16:43:26 +0200

    Create QProcessor

--- src/main/java/dev/apt/searchengine/QueryProcessor/QueryProcessor.java
+++ src/main/java/dev/apt/searchengine/QueryProcessor/QueryProcessor.java
@@ -1,19 +1,29 @@
 package dev.apt.searchengine.QueryProcessor;
 
+import java.util.LinkedList;
+import dev.apt.searchengine.Crawler.CrawlerDB;
+
 import org.springframework.data.mongodb.core.aggregation.VariableOperators.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.apt.searchengine.Indexer.DocData;
+import dev.apt.searchengine.Indexer.WordsProcessor;
+
 @RestController
 @RequestMapping("/api/v1/query")
 public class QueryProcessor {
-    String query;
     @PostMapping
-    public String processQuery(@RequestBody String query) {
-        this.query = query;
-        System.out.println(query);
-        return "Query processed";
+    public LinkedList<String> processQuery(@RequestBody String query) {
+        query = WordsProcessor.withoutStopWords(query);
+        String[] words = query.split(" ");
+        for(String word : words) {
+            word = WordsProcessor.wordStemmer(word);
+        }
+        LinkedList<String> urls = new LinkedList<>();
+        //ranker.rank(words, urls);
+        return urls;
     }
 }
